30ee780feb9f83d75060ec6344d6a42fd49ee0e0
Mutwoki Dennis Mureti <dennis.mutwoki@strathmore.edu>
2024-01-22 17:30:15 +0300

    nnak

--- app/(associations)/_layout.tsx
+++ app/(associations)/_layout.tsx
@@ -1,29 +1,46 @@
-import { Stack } from 'expo-router';
-import React from 'react';
-import ProfileHeaderLeft from '../../components/profile/HeaderLeft';
+import { Stack } from "expo-router";
+import React from "react";
+import ProfileHeaderLeft from "../../components/profile/HeaderLeft";
 
 const StudentsLayout = () => {
-	return (
-		<Stack
-			screenOptions={{
-				headerTitleAlign: 'center',
-				headerStyle: {
-					backgroundColor: '#3c6470',
-				},
+  return (
+    <Stack screenOptions={{}}>
+      <Stack.Screen
+        name="kpna"
+        options={{
+          title: "KPNA",
+          headerLeft: () => <ProfileHeaderLeft />,
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#3c6470",
+          },
 
-				headerTitleStyle: {
-					color: '#FFF',
-				},
-			}}>
-			<Stack.Screen
-				name='kpna'
-				options={{
-					title: 'KPNA',
-					headerLeft: () => <ProfileHeaderLeft />,
-				}}
-			/>
-		</Stack>
-	);
+          headerTitleStyle: {
+            color: "#FFF",
+          },
+        }}
+      />
+      <Stack.Screen
+        name="nnak"
+        options={{
+          title: "NNAK",
+          headerLeft: () => <ProfileHeaderLeft />,
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#FF3C33",
+          },
+
+          headerTitleStyle: {
+            color: "#FFF",
+          },
+          contentStyle: {
+            borderTopColor: "#00ff00",
+            borderTopWidth: 2,
+          },
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default StudentsLayout;
