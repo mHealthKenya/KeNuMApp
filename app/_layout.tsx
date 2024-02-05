4d9464a127c3e1e2d08d5df7ed0693ad9a514ec2
JoelWekesa <joelwekesa.jw@gmail.com>
2024-02-05 16:06:34 +0300

    Added EAS update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot, Stack } from 'expo-router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import AuthProvider from '../providers/auth';
 import ErrorProvider from '../providers/error';
 import { ThemeProvider } from '@rneui/themed';
@@ -11,10 +11,29 @@ import RotationAreasProvider from '../providers/rotationareas';
 import RotationCompetenciesProvider from '../providers/rotationcompetencies';
 import CompetencyProvider from '../providers/competency';
 import RegistrationProvider from '../providers/registrationprovider';
+import * as Updates from 'expo-updates';
 
 const client = new QueryClient();
 
 const RootLayout = () => {
+	async function onFetchUpdateAsync() {
+		try {
+			const update = await Updates.checkForUpdateAsync();
+
+			if (update.isAvailable) {
+				await Updates.fetchUpdateAsync();
+				await Updates.reloadAsync();
+			}
+		} catch (error) {
+			// You can also add an alert() to see the error message in case of an error when fetching updates.
+			return;
+		}
+	}
+
+	useEffect(() => {
+		onFetchUpdateAsync();
+	}, []);
+
 	return (
 		<GluestackUIProvider config={config}>
 			<ThemeProvider>
