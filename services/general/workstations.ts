640a798c219d81e6eecd5cbe4082257228b328c1
Joel Wekesa <joelwekesa@Joels-MacBook-Air-2.local>
2024-03-13 13:08:03 +0300

    Private practice apply

--- services/general/workstations.ts
+++ services/general/workstations.ts
@@ -19,11 +19,32 @@ const workStations = async (id: string) => {
 	return response;
 };
 
+const workStationsAll = async () => {
+	const token = await secureStore.getItemAsync('token').then((data) => data);
+	axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+	const url = baseUrl + 'api/workstations/all';
+
+	const config: AxiosRequestConfig = {
+		method: 'GET',
+		url,
+	};
+
+	const response: WorkStation[] = await axios(config).then((res) => res.data);
+
+	return response;
+};
+
 const useWorkStations = (id: string) =>
 	useQuery({
 		queryKey: ['workstations', id],
 		queryFn: () => workStations(id),
 		enabled: !!id,
 	});
+
+
+export const useAllWorkStations = () => useQuery({
+	queryKey: ['workstations-all'],
+	queryFn: workStationsAll
+})
 
 export default useWorkStations;
