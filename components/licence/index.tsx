0da9f51673f83b77ce3d2653da32a912c317e7ab
Guyoguyes <g.abduba43@gmail.com>
2024-11-25 15:40:10 +0300

    commit

--- components/licence/index.tsx
+++ components/licence/index.tsx
@@ -7,6 +7,7 @@ import {LicenceApplication} from '../../models/licenceapplications';
 import useAuthenticatedUser from '../../services/auth/authenticated';
 import {ActivityIndicator} from 'react-native-paper';
 import {primaryColor} from '../../constants/Colors';
+import DownloadBox from './downloadbox';
 
 interface Props {
 	applications: LicenceApplication[];
@@ -18,10 +19,14 @@ const LicenceHomeComponent: FC<Props> = ({applications}) => {
 	if (isLoading) {
 		return (
 			<View style={[globalStyles.container, globalStyles.center]}>
-				<ActivityIndicator size="large" color={primaryColor} />
+				<ActivityIndicator size='large' color={primaryColor} />
 			</View>
 		);
 	}
+
+	const hasActiveLicence = data?.license?.some((l) => {
+		return l.to_date ? new Date(l.to_date) > new Date() : false;
+	});
 
 	const hasRequisiteCPD = data?.cpd?.every((c) => {
 		return c.cpd_requirement && c.current_points ? +c.current_points >= +c.cpd_requirement : false;
@@ -34,7 +39,17 @@ const LicenceHomeComponent: FC<Props> = ({applications}) => {
 	return (
 		<View style={globalStyles.container}>
 			<ScrollView style={{flex: 1}}>
-				{!hasRequisiteCPD ? (
+				{hasActiveLicence ? (
+					<LBox
+						box={{
+							title: 'Licence Renewal',
+							content: 'Complete your annual licence renewal',
+							backgroundColor: '#dcf0fa',
+							path: require('../../assets/images/licencesmall.png'),
+							route: '/licencecountry',
+						}}
+					/>
+				) : !hasRequisiteCPD ? (
 					<LBox
 						box={{
 							title: 'Complete CPDs',
